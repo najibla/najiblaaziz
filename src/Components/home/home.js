b7946e293cdd834c6523f66335423905af7ed873
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2018-11-29 11:15:06 +0200

    added tslint prettier

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,14 +1,17 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
 
 export default class home extends PureComponent {
   render() {
     return (
-        <div style={{"textAlign": "center"}} className="mt-3">
-            <h1>
-                Welcome!
-            </h1>
-            <img className="mt-3" width="300" alt="Angular Logo" src={require("./../../assets/images/toughviking.jpg")} />
-        </div>
-    )
+      <div style={{ textAlign: 'center' }} className="mt-3">
+        <h1>Welcome!</h1>
+        <img
+          className="mt-3"
+          width="300"
+          alt="Angular Logo"
+          src={require('./../../assets/images/toughviking.jpg')}
+        />
+      </div>
+    );
   }
 }
