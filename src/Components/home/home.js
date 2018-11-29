1517ec7937605533d77c2c305b546e003862e020
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2018-11-30 01:17:09 +0200

    cat api

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -16,11 +16,15 @@ export default class home extends PureComponent {
         });
       });
   }
+
+  reload = () => {
+    window.location.reload();
+  };
   render() {
     const { cats } = this.state;
     return (
       <div style={{ textAlign: 'center' }} className="mt-3">
-        <h1 className="mb-3">I am a beautiful cat!</h1>
+        <h1 className="mb-3">Hello, I am a cat!</h1>
         {cats.map(item => (
           <img
             className="col-xs-12 col-sm-8 col-md-7"
@@ -29,6 +33,11 @@ export default class home extends PureComponent {
             key={item.id}
           />
         ))}
+        <div className="row mt-3">
+          <button style={{ margin: '0 auto' }} href="#" onClick={this.reload}>
+            Click me to see a new image
+          </button>
+        </div>
       </div>
     );
   }
