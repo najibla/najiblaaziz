496be26f420c5b991aeecc99eea9ae8f09c76b48
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2019-10-30 21:43:18 +0200

    added loader

--- src/Components/gallery/gallery.js
+++ src/Components/gallery/gallery.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { ReactBnbGallery } from 'react-bnb-gallery';
 import { getName } from 'country-list';
 import './gallery.scss';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { textAlign } from '@material-ui/system';
 
 const Gallery = props => {
   const [photos, setPhotos] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const res = await fetch(
         `https://dev.api.najiblaaziz.com/${props.countryClicked}`
       );
@@ -18,8 +22,10 @@ const Gallery = props => {
             Object.assign({}, { photo: src, thumbnail: thumbnail })
           );
           setPhotos(res);
+          setLoading(false);
         })
         .catch(err => {
+          setLoading(false);
           setError(true);
           console.log(err);
         });
@@ -35,11 +41,28 @@ const Gallery = props => {
     <div className="coming-soon">
       Pictures from {getName(props.countryClicked)} Coming soon!
     </div>
-  ) : !(props.countryClicked && photos.length) ? (
-    <></>
+  ) : !loading ? (
+    !(props.countryClicked && photos.length) ? (
+      <></>
+    ) : (
+      <div>
+        <ReactBnbGallery show={true} photos={photos} onClose={toggleGallery} />
+      </div>
+    )
   ) : (
-    <div>
-      <ReactBnbGallery show={true} photos={photos} onClose={toggleGallery} />
+    <div
+      style={{
+        textAlign: 'center',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: '1px',
+        margin: 'auto'
+      }}
+    >
+      <CircularProgress color="primary" />
     </div>
   );
 };
