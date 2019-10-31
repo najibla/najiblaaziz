159b059d7e7f88fe09ce41eb16e216b2f2661588
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2019-10-31 12:08:49 +0200

    added question mark

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HomeOutlined from '@material-ui/icons/HomeOutlined';
+import HelpOutlineOutlined from '@material-ui/icons/HelpOutlineOutlined';
 import './header.css';
+import { Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { flexbox } from '@material-ui/system';
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}));
 
 const Header = () => {
+  const classes = useStyles();
   const reload = () => window.location.reload();
-
+  const [questionModalOpen, setModalOpen] = useState(false);
   return (
-    <header>
-      <nav>
-        <ul style={{ textAlign: 'center' }}>
-          <li>
-            <Link to="/" onClick={reload}>
-              <HomeOutlined fontSize="large" color="primary" />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <ul style={{ textAlign: 'center' }}>
+            <li
+              style={{
+                display: 'flex',
+                justifyContent: 'start'
+              }}
+            >
+              <Link
+                to="/"
+                onClick={reload}
+                style={{
+                  marginRight: '30px'
+                }}
+              >
+                <HomeOutlined fontSize="large" color="primary" />
+              </Link>
+              <HelpOutlineOutlined
+                fontSize="large"
+                color="primary"
+                style={{
+                  cursor: 'pointer'
+                }}
+                onClick={() => setModalOpen(true)}
+              />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Modal
+        open={questionModalOpen}
+        onClose={() => setModalOpen(false)}
+        className={classes.modal}
+      >
+        <div className={classes.paper}>
+          <h2 id="simple-modal-title">What is this?</h2>
+          <p id="simple-modal-description">
+            In this page you can see all countries I have been to marked with
+            different colors.
+            <br />
+            <br />
+            Click on one of the countries to see pictures from there.
+          </p>
+        </div>
+      </Modal>
+    </>
   );
 };
 
